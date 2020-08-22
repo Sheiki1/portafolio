682304d4343fed4b10f2a56c165dcfa309a6c0a9
Sheiki1 <kirokomerteabesos@gmail.com>
2020-08-22 16:27:44 -0500

    Updates

---
+++ precache-manifest.3e9a93a44f41c4a392e94cd367e1677e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7508fa6ec8efc55c548e774905c7a544",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "f3ef9097db95ce6e7938",
+    "url": "/portafolio/static/css/main.7976d99c.chunk.css"
+  },
+  {
+    "revision": "abc22450b73d8853dd7a",
+    "url": "/portafolio/static/js/2.b6d4933d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portafolio/static/js/2.b6d4933d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ef9097db95ce6e7938",
+    "url": "/portafolio/static/js/main.96f280a2.chunk.js"
+  },
+  {
+    "revision": "11f9e52ab9233dae687b",
+    "url": "/portafolio/static/js/runtime-main.6d304cc1.js"
+  },
+  {
+    "revision": "68a0ede7099c2a14d1aaabaeadf0cd2f",
+    "url": "/portafolio/static/media/photo.68a0ede7.jpg"
+  },
+  {
+    "revision": "d2ba35dd6e783b69747fa9f736be3cb9",
+    "url": "/portafolio/static/media/triangulo1.d2ba35dd.png"
+  },
+  {
+    "revision": "1d8d86868e2e20d8d83b59d149950517",
+    "url": "/portafolio/static/media/triangulo2.1d8d8686.png"
+  }
+]);
